9ad8f780ffe46ed7b5466ccffd047e43cb0fcfb7
Joan Gerard <joan.gerard@outlook.com>
2022-09-22 14:33:41 +0200

    project setup

---
+++ serverless.ts
@@ -0,0 +1,66 @@
+import type { AWS } from "@serverless/typescript";
+
+import functions from "./serverless/functions";
+import dynamoResources from "./serverless/dynamoResources";
+
+const serverlessConfiguration: AWS = {
+  service: "reminderapp",
+  frameworkVersion: "3",
+  plugins: ["serverless-esbuild"],
+  provider: {
+    name: "aws",
+    runtime: "nodejs14.x",
+    profile: "serverlessUser",
+    region: "eu-central-1",
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: "dynamodb:*",
+        Resource:
+          "arn:aws:dynamodb:${self:provider.region}:${aws:accountId}:table/${self:custom.reminderTable}",
+      },
+    ],
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+      reminderTable: "${self:custom.reminderTable}",
+      baseUrl: {
+        "Fn::Join": [
+          "",
+          [
+            "https://",
+            { Ref: "HttpApi" },
+            ".execute-api.${self:provider.region}.amazonaws.com",
+          ],
+        ],
+      },
+    },
+  },
+  // import the function via paths
+  functions,
+  resources: {
+    Resources: {
+      ...dynamoResources,
+    },
+  },
+  package: { individually: true },
+  custom: {
+    reminderTable: "${sls:stage}-reminder-table",
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
+      concurrency: 10,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
